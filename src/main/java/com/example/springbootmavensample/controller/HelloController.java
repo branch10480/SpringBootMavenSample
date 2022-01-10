c3015b3468e1b6518664d3dbdf6beef022497b9a
branch10480 <toshiharu.imaeda@gmail.com>
2022-01-10 15:41:28 +0900

    Postパラメータ対応

--- src/main/java/com/example/springbootmavensample/controller/HelloController.java
+++ src/main/java/com/example/springbootmavensample/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springbootmavensample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,22 +12,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    // HTTPリクエストを受け付ける
-    // 引数に "/" を指定したので http://localhost:8080/ でアクセス可能
-    @RequestMapping("/")
-    public String index() {
-        // hello.htmlを呼び出します
-        // templatesフォルダの hello.html を呼び出せます
-        return "hello";
-    }
+  // HTTPリクエストを受け付ける
+  // 引数に "/" を指定したので http://localhost:8080/ でアクセス可能
+  @RequestMapping("/")
+  public String index() {
+    // hello.htmlを呼び出します
+    // templatesフォルダの hello.html を呼び出せます
+    return "hello";
+  }
 
-    // HTTPのGETリクエストを受け付けます
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(required = false) String message, Model model) {
-        // Thymeleafへ取得した値を渡すにはModelへ値を追加します
-        model.addAttribute("sample", message);
-        return "hello";
-    }
+  // HTTPのGETリクエストを受け付けます
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(required = false) String message, Model model) {
+    // Thymeleafへ取得した値を渡すにはModelへ値を追加します
+    model.addAttribute("sample", message);
+    return "hello";
+  }
+
+  @RequestMapping("/form")
+  public String form() {
+    return "form";
+  }
+
+  @PostMapping("/confirm")
+  public String confirm(@RequestParam String message, Model model) {
+    model.addAttribute("message", message);
+    return "confirm";
+  }
 }
 
 /*
