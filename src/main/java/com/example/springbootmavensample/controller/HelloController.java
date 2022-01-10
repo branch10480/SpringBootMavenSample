0516638a41cee05a842c02d3a55fc87332452850
branch10480 <toshiharu.imaeda@gmail.com>
2022-01-10 15:24:23 +0900

    Getパラメータ対応

--- src/main/java/com/example/springbootmavensample/controller/HelloController.java
+++ src/main/java/com/example/springbootmavensample/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.springbootmavensample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // @~はアノテーション（注釈）プログラムに関する補足情報を提供します
 // このクラスがコントローラーであることを示します
@@ -16,6 +19,14 @@ public class HelloController {
         // templatesフォルダの hello.html を呼び出せます
         return "hello";
     }
+
+    // HTTPのGETリクエストを受け付けます
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String message, Model model) {
+        // Thymeleafへ取得した値を渡すにはModelへ値を追加します
+        model.addAttribute("sample", message);
+        return "hello";
+    }
 }
 
 /*
@@ -27,4 +38,14 @@ DTO -> dto, myApp -> myapp  ...etc
 Thymeleaf（タイムリーフ）はSpring Bootで推奨されているテンプレートエンジン
 昔はJSP（JavaServer Pages）が主流でしたが、今は非推奨。
 JSPを知っている場合であってもThymeleafを使います。
+ */
+
+/*
+@RequestParamには引数を渡すことができる
+ちなみにrequiredは指定しなければtrue
+
+e.g.)
+@RequestParam("message")
+@RequestParam(name = "message")
+@RequestParam(defaultValue = "Hi!")
  */
